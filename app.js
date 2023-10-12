8606d244f0ff173990e9baee8a737273a800df6a
cwatchley4 <cwatchley4@gmail.com>
2023-10-12 10:13:44 -0500

    Update JS
    Updated code for drop down menu toggle. Added functionality to click outside of dropdown menu to close drop down menu.

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const toggleBtn = document.querySelector(".toggle-btn")
-        const toggleBtnIcon = document.querySelector(".toggle-btn i")
-        const dropDownMenu = document.querySelector(".dropdown-menu")
+"use strict";
 
-        toggleBtn.onclick = function () {
-            dropDownMenu.classList.toggle("open")
-            const isOpen = dropDownMenu.classList.contains("open")
+// Responsive Nav Bar
 
-            toggleBtnIcon.classList = isOpen
-            ? "fa-solid fa-xmark"
-            : "fa-solid fa-bars"
-        }
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnIcon = document.querySelector(".toggle-btn i");
+const dropDownMenu = document.querySelector(".dropdown-menu");
+
+const changeDropDownMenuIcon = function () {
+  const isOpen = dropDownMenu.classList.contains("open");
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
+
+const toggleDropDownMenu = function () {
+  dropDownMenu.classList.toggle("open");
+};
+
+toggleBtn.addEventListener("click", function () {
+  toggleDropDownMenu();
+  changeDropDownMenuIcon();
+});
+
+document.querySelector("main").addEventListener("click", function () {
+  dropDownMenu.classList.remove("open");
+  changeDropDownMenuIcon();
+});
