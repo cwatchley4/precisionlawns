9a58e0dec60e349ec8c3c7ec241a13d0b015e674
cwatchley4 <cwatchley4@gmail.com>
2024-02-13 10:14:05 -0600

    Updated Modal
    Changed to spring theme.

--- app.js
+++ app.js
@@ -40,175 +40,145 @@ const closeModal = function () {
 console.log(sessionStorage);
 
 const openModal = function () {
+  document.querySelector("body").classList.add("no-scroll");
   modal.classList.remove("hidden");
   modalOverlay.classList.remove("hidden");
-  document.querySelector("body").classList.add("no-scroll");
+  const expirationTime = 60 * 60 * 1000; // 1 hour in milliseconds
+  const currentTime = new Date().getTime();
+  localStorage.setItem(
+    "viewedModal",
+    JSON.stringify({ value: "true", expiry: currentTime + expirationTime })
+  );
 };
 
 // DOM loads the modal
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (!sessionStorage.getItem("viewedModal")) openModal();
-  sessionStorage.setItem("viewedModal", "true");
+  const viewedModal = JSON.parse(localStorage.getItem("viewedModal"));
+  if (!viewedModal || viewedModal.expiry < new Date().getTime()) {
+    openModal();
+  }
 });
 
 // Close button, click body, and escape key closes the modal
 
-if (document.querySelector(".modal")) {
-  if (!modal.classList.contains("hidden")) {
-    closeModalButton.addEventListener("click", function () {
-      closeModal();
-    });
-    document.querySelector("body").addEventListener("click", function () {
-      closeModal();
-    });
-  }
-}
+closeModalButton.addEventListener("click", function () {
+  if (!modal.classList.contains("hidden")) closeModal();
+  console.log("clicked");
+});
+document.querySelector("body").addEventListener("click", function () {
+  if (!modal.classList.contains("hidden")) closeModal();
+});
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
 });
 
-// Snow Removal Calculator
+// BLOG PAGE
 
-/*
+if (document.querySelector("[page='blog']")) {
+  document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.querySelector(".search-form input");
+    const sortButtonContainer = document.querySelector(".sort-btn-container");
+    const clearButton = document.querySelector(".clear-btn");
+    const blogPostsContainer = document.querySelector(".grid-container");
 
-const lengthOfDriveway = document.querySelector("#length");
-const inchesOfSnow = document.querySelector("#inches");
-const saltingCheck = document.querySelector("#checkbox");
-const calcButton = document.querySelector(".calculate-button");
-const cost = document.querySelector(".cost");
-const message = document.querySelector(".message");
+    // Extract and split search terms from data attribute
+    const extractSearchTerms = (post) => {
+      const dataAttribute = post.getAttribute("search-terms");
+      return dataAttribute
+        ? dataAttribute
+            .toLowerCase()
+            .split(",")
+            .map((term) => term.trim())
+        : [];
+    };
 
-const calcPlowDrive = function () {
-  if (!lengthOfDriveway.value || !inchesOfSnow.value) {
-    message.textContent = "Enter numbers for driveway length and snow depth!";
-    return NaN;
-  } else {
-    message.textContent = "Your snow removal would cost about:";
-    if (cost.classList.contains("hidden")) cost.classList.remove("hidden");
-    if (inchesOfSnow.value <= 6) {
-      cost.textContent = `$${65}`;
-      return 65;
-    } else {
-      cost.textContent = `$${95}`;
-      return 95;
-    }
-  }
-};
+    // Function to compare blog post dates for sorting
+    const compareDates = (a, b) => {
+      const dateA = new Date(a.getAttribute("date"));
+      const dateB = new Date(b.getAttribute("date"));
 
-calcButton.addEventListener("click", function () {
-  if (!saltingCheck.checked) {
-    calcPlowDrive();
-  } else {
-    const costWithoutSalting = calcPlowDrive();
-    cost.textContent = `$${costWithoutSalting + costWithoutSalting * 0.1}`;
-  }
-});
+      return dateB - dateA;
+    };
 
-*/
-
-document.addEventListener("DOMContentLoaded", function () {
-  const searchInput = document.querySelector(".search-form input");
-  const sortButtonContainer = document.querySelector(".sort-btn-container");
-  const clearButton = document.querySelector(".clear-btn");
-  const blogPostsContainer = document.querySelector(".grid-container");
-
-  // Extract and split search terms from data attribute
-  const extractSearchTerms = (post) => {
-    const dataAttribute = post.getAttribute("search-terms");
-    return dataAttribute
-      ? dataAttribute
-          .toLowerCase()
-          .split(",")
-          .map((term) => term.trim())
-      : [];
-  };
-
-  // Function to compare blog post dates for sorting
-  const compareDates = (a, b) => {
-    const dateA = new Date(a.getAttribute("date"));
-    const dateB = new Date(b.getAttribute("date"));
-
-    return dateB - dateA;
-  };
-
-  // Initial sorting by newest first
-  let originalBlogPosts = [...blogPostsContainer.children];
-  blogPostsContainer.innerHTML = originalBlogPosts
-    .sort(compareDates)
-    .map((post) => post.outerHTML)
-    .join("");
-
-  // Add click event to the sort button
-  sortButtonContainer.addEventListener("click", function () {
-    const isSortedByNewest =
-      sortButtonContainer.querySelector("span").textContent ===
-      "Sort by Newest";
-
-    // Toggle between sorting by newest and sorting by oldest
-    const sortedPosts = originalBlogPosts
-      .slice()
-      .sort(isSortedByNewest ? compareDates : (a, b) => -compareDates(a, b));
-
-    // Update the blog posts container with the sorted posts
-    blogPostsContainer.innerHTML = sortedPosts
-      .map((post) => post.outerHTML)
-      .join("");
-
-    // Change the text of the sort button
-    sortButtonContainer.querySelector("span").textContent = isSortedByNewest
-      ? "Sort by Oldest"
-      : "Sort by Newest";
-
-    // Hide the clear button
-    clearButton.classList.add("hidden");
-  });
-
-  searchInput.addEventListener("input", function () {
-    const searchInputTrimmed = searchInput.value.toLowerCase().trim(); // Trim spaces
-    const searchInputWords = searchInputTrimmed.split(/\s+/); // Split by whitespace
-
+    // Initial sorting by newest first
+    let originalBlogPosts = [...blogPostsContainer.children];
     blogPostsContainer.innerHTML = originalBlogPosts
+      .sort(compareDates)
       .map((post) => post.outerHTML)
       .join("");
 
-    blogPostsContainer.childNodes.forEach((post) => {
-      const searchTerms = extractSearchTerms(post);
-      const title = post
-        .querySelector(".blog-title")
-        .textContent.toLowerCase()
-        .trim(); // Trim spaces
+    // Add click event to the sort button
+    sortButtonContainer.addEventListener("click", function () {
+      const isSortedByNewest =
+        sortButtonContainer.querySelector("span").textContent ===
+        "Sort by Newest";
 
-      // Check if any word of the search input matches any search term or the title
-      const matches = searchInputWords.some(
-        (word) =>
-          searchTerms.some((term) => term.includes(word)) ||
-          title.includes(word)
-      );
+      // Toggle between sorting by newest and sorting by oldest
+      const sortedPosts = originalBlogPosts
+        .slice()
+        .sort(isSortedByNewest ? compareDates : (a, b) => -compareDates(a, b));
 
-      if (!matches) {
-        post.style.display = "none";
-      }
+      // Update the blog posts container with the sorted posts
+      blogPostsContainer.innerHTML = sortedPosts
+        .map((post) => post.outerHTML)
+        .join("");
+
+      // Change the text of the sort button
+      sortButtonContainer.querySelector("span").textContent = isSortedByNewest
+        ? "Sort by Oldest"
+        : "Sort by Newest";
+
+      // Hide the clear button
+      clearButton.classList.add("hidden");
     });
 
-    // Show the clear button
-    clearButton.classList.remove("hidden");
-    sortButtonContainer.classList.add("hidden");
+    searchInput.addEventListener("input", function () {
+      const searchInputTrimmed = searchInput.value.toLowerCase().trim(); // Trim spaces
+      const searchInputWords = searchInputTrimmed.split(/\s+/); // Split by whitespace
+
+      blogPostsContainer.innerHTML = originalBlogPosts
+        .map((post) => post.outerHTML)
+        .join("");
+
+      blogPostsContainer.childNodes.forEach((post) => {
+        const searchTerms = extractSearchTerms(post);
+        const title = post
+          .querySelector(".blog-title")
+          .textContent.toLowerCase()
+          .trim(); // Trim spaces
+
+        // Check if any word of the search input matches any search term or the title
+        const matches = searchInputWords.some(
+          (word) =>
+            searchTerms.some((term) => term.includes(word)) ||
+            title.includes(word)
+        );
+
+        if (!matches) {
+          post.style.display = "none";
+        }
+      });
+
+      // Show the clear button
+      clearButton.classList.remove("hidden");
+      sortButtonContainer.classList.add("hidden");
+    });
+
+    // Add click event to clear the search input
+    clearButton.addEventListener("click", function () {
+      searchInput.value = ""; // Clear the search input
+      clearButton.classList.add("hidden"); // Hide the clear button
+      sortButtonContainer.classList.remove("hidden");
+
+      // Show all blog posts
+      blogPostsContainer.innerHTML = originalBlogPosts
+        .map((post) => post.outerHTML)
+        .join("");
+
+      // Change the text of the sort button to "Sort by Newest"
+      sortButtonContainer.querySelector("span").textContent = "Sort by Newest";
+    });
   });
-
-  // Add click event to clear the search input
-  clearButton.addEventListener("click", function () {
-    searchInput.value = ""; // Clear the search input
-    clearButton.classList.add("hidden"); // Hide the clear button
-    sortButtonContainer.classList.remove("hidden");
-
-    // Show all blog posts
-    blogPostsContainer.innerHTML = originalBlogPosts
-      .map((post) => post.outerHTML)
-      .join("");
-
-    // Change the text of the sort button to "Sort by Newest"
-    sortButtonContainer.querySelector("span").textContent = "Sort by Newest";
-  });
-});
+}
